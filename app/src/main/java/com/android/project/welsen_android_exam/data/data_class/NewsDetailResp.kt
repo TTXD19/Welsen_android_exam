5f52dd9bf83ef67f5e4298791da966ec60253dd5
welsen <welsen9595@gmail.com>
2021-09-05 12:54:29 +0800

    [feature] Code refactor

--- app/src/main/java/com/android/project/welsen_android_exam/data/data_class/NewsDetailResp.kt
+++ app/src/main/java/com/android/project/welsen_android_exam/data/data_class/NewsDetailResp.kt
@@ -1,8 +1,6 @@
 package com.android.project.welsen_android_exam.data.data_class
 
 
-import com.google.gson.annotations.SerializedName
-
 data class NewsDetailResp(
     val articles: List<Article>? = null,
     val status: String? = null,
